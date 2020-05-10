e2559749412a8fafae6c7784f8b73d40631a8661
Manikanth <65079433+alpha-neutr0n@users.noreply.github.com>
2020-05-10 18:30:38 +0530

    Create Snake.java

---
+++ OOPs/5.Interfaces/Snake.java
@@ -0,0 +1,27 @@
+public class Snake implements TiredAnimal {
+
+    @Override
+    public void whatsMyName() {
+        System.out.println("Snake: Hi there! I am Python.");
+    }
+    @Override
+    public void eat() {
+        System.out.println("Snake: I land in category of snakes and I swallow my preys.");
+    }
+
+    @Override
+    public void howManyLegs() {
+        System.out.println("Snake: I don't have legs.");
+    }
+
+
+    @Override
+    public void produceSound() {
+        System.out.println("Snake: I produce Hissing sound.");
+    }
+
+    @Override
+    public void whenTired() {
+        System.out.println("Snake: I too love sleeping.");
+    }
+}
