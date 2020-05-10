0a4084ca71dfb825c5ba8203c6d5974fe2199f54
Manikanth <65079433+alpha-neutr0n@users.noreply.github.com>
2020-05-10 19:01:46 +0530

    Update Program10a.java

--- OOPs/10.String Handling/Program10a.java
+++ OOPs/10.String Handling/Program10a.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Pgm10a{
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.print("Input a Sentence: ");
