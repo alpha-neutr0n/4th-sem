bc2c001a4adcb7afedeb54cc0af6c27ef2fc71b8
Manikanth <65079433+alpha-neutr0n@users.noreply.github.com>
2020-05-10 18:29:42 +0530

    Create Main.java

---
+++ OOPs/5.Interfaces/Main.java
@@ -0,0 +1,34 @@
+interface Animal{
+    void eat();
+    void howManyLegs();
+    void whatsMyName();
+}
+
+interface TiredAnimal extends Animal{
+    void produceSound();
+    void whenTired();
+}
+
+public class Main{
+    public static void main(String args[]){
+        Lion lion = new Lion();
+        Snake snake = new Snake();
+
+        //methods in Animal Interface
+        lion.whatsMyName();
+        snake.whatsMyName();
+
+        lion.eat();
+        snake.eat();
+
+        lion.howManyLegs();
+        snake.howManyLegs();
+
+        //methods in TiredAnimal Interface
+        lion.produceSound();
+        snake.produceSound();
+
+        lion.whenTired();
+        snake.whenTired();
+    }
+}
