2461968417602981781eb3a00b21874afdde98d4
Manikanth <65079433+alpha-neutr0n@users.noreply.github.com>
2020-06-03 19:34:33 +0530

    Create Cse_dept.java

---
+++ OOPs/4.Packages/Cse_dept.java
@@ -0,0 +1,13 @@
+//This code contains class Cse_dept extending Thirdsem
+//This code imports CSE package.
+
+package RVCE;
+import CSE.*;
+public class Cse_dept extends Thirdsem
+{
+    // Member functions
+    @Override
+    private void Welcomemsg() {
+        super.Welcomemsg();
+    }
+}
