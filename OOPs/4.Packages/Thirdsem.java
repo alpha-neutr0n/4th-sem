272595135cb76135805a27659699ff5acadefd4c
Manikanth <65079433+alpha-neutr0n@users.noreply.github.com>
2020-06-03 19:32:37 +0530

    Create Thirdsem.java

---
+++ OOPs/4.Packages/Thirdsem.java
@@ -0,0 +1,11 @@
+//This code contains class Thirdsem under package named CSE
+package CSE;
+
+public class Thirdsem
+{
+    // Member functions
+    public void Welcomemsg()
+    {
+        System.out.println("Welcome to CSE dept - 4th sem young budding Engineers");
+    }
+}
