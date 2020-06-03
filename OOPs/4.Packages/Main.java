dec2402b5c37618c0b008cce1a302db8b4121c74
Manikanth <65079433+alpha-neutr0n@users.noreply.github.com>
2020-06-03 19:35:48 +0530

    Create Main.java

---
+++ OOPs/4.Packages/Main.java
@@ -0,0 +1,12 @@
+//This code is used to test the usage of packages
+
+import RVCE.Cse_dept;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Cse_dept obj = new Cse_dept();
+        obj.Welcomemsg();
+    }
+}
